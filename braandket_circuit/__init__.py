157499037c57bd5c72b2ccdb068cf1bcb7681ef1
keli <zhengkeli2009@126.com>
2023-08-11 11:36:10 +0800

    added basic definition of QOperation

--- braandket_circuit/__init__.py
+++ braandket_circuit/__init__.py
@@ -1,1 +1,3 @@
+from .basics import *
+
 __version__ = '0.1.0-dev'
---
+++ braandket_circuit/basics/operation.py
@@ -0,0 +1,17 @@
+import abc
+from typing import Optional
+
+from braandket import QSystem
+
+
+class QOperation(abc.ABC):
+    def __init__(self, *, name: Optional[str] = None):
+        self._name = name
+
+    @property
+    def name(self) -> Optional[str]:
+        return self._name
+
+    @abc.abstractmethod
+    def __call__(self, *args: QSystem, **kwargs: QSystem):
+        pass
