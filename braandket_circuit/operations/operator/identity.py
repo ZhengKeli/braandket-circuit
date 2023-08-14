c8f8ab243a94d3fd27c9898c43fa6c5e6ee5b952
keli <zhengkeli2009@126.com>
2023-08-14 14:06:35 +0800

    let _IdentityOperation be a subclass of MatrixOperation

--- braandket_circuit/operations/operator/identity.py
+++ braandket_circuit/operations/operator/identity.py
@@ -1,9 +1,23 @@
-from braandket_circuit.basics import QOperation
-from braandket_circuit.system import QSystem
+import numpy as np
+
+import braandket as bnk
+from braandket import BackendValue, OperatorTensor
+from braandket_circuit.system import QComposed, QSystem
+from .matrix import MatrixOperation
 
 
-class _IdentityOperation(QOperation):
+class _IdentityOperation(MatrixOperation):
     """ Operation that does nothing """
+
+    def make_matrix(self, *systems: QSystem) -> BackendValue:
+        system = QComposed(*systems)
+        N = int(np.prod([space.n for space in system.spaces]))
+        return system.backend.eye(N)
+
+    def make_operator_tensor(self, *systems: QSystem) -> OperatorTensor:
+        system = QComposed(*systems)
+        spaces_identity = [space.identity(backend=system.backend) for space in system.spaces]
+        return OperatorTensor.of(bnk.prod(*spaces_identity, backend=system.backend))
 
     def __call__(self, *args: QSystem, **kwargs: QSystem):
         pass
