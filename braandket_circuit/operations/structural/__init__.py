960af6542bbb4dc77b02c6cb0bb2c64832c71d12
keli <zhengkeli2009@126.com>
2023-08-17 14:42:52 +0800

    redesigned Remapped, added RemappedByLambda and RemappedByIndices

--- braandket_circuit/operations/structural/__init__.py
+++ braandket_circuit/operations/structural/__init__.py
@@ -1,3 +1,3 @@
 from .controlled import Controlled
-from .remapped import Remapped
+from .remapped import Remapped, RemappedByIndices, RemappedByLambda
 from .sequential import Sequential
--- braandket_circuit/operations/structural/remapped.py
+++ braandket_circuit/operations/structural/remapped.py
@@ -4,31 +4,60 @@ from typing import Callable, Generic, Optional, ParamSpec, TypeVar
 from braandket_circuit.basics import QOperation, QSystem, QSystemStruct
 
 Op = TypeVar('Op', bound=QOperation)
-QSystemsArgs = QSystemStruct
 QSystemSpec = ParamSpec('QSystemSpec', bound=QSystemStruct)
 
 
 class Remapped(QOperation, Generic[Op], abc.ABC):
-    def __init__(self, op: Op, mapper: Callable[QSystemSpec, QSystemsArgs], *, name: Optional[str] = None):
+    def __init__(self, op: Op, *, name: Optional[str] = None):
         super().__init__(name=name)
-
-        # check
-        if not isinstance(op, QOperation):
-            raise TypeError(f"op={op} is not a QOperation!")
-
         self._op = op
-        self._mapper = mapper
+
+    @property
+    def op(self) -> Op:
+        return self._op
+
+    @abc.abstractmethod
+    def remap(self, *args: QSystemStruct) -> QSystemStruct:
+        pass
+
+    def __call__(self, *args: QSystemStruct):
+        mapped_args = self.remap(*args)
+        if isinstance(mapped_args, QSystem):
+            mapped_args = (mapped_args,)
+        return self.op(*mapped_args)
+
+
+class RemappedByLambda(Remapped[Op]):
+    def __init__(self, op: Op, func: Callable[QSystemSpec, QSystemStruct], *, name: Optional[str] = None):
+        super().__init__(op, name=name)
+        self._func = func
 
     @property
     def op(self) -> Op:
         return self._op
 
     @property
-    def mapper(self) -> Callable[QSystemSpec, QSystemsArgs]:
-        return self._mapper
+    def func(self) -> Callable[QSystemSpec, QSystemStruct]:
+        return self._func
 
-    def __call__(self, *args: QSystemStruct):
-        mapped_args = self.mapper(*args)
-        if isinstance(mapped_args, QSystem):
-            mapped_args = (mapped_args,)
-        return self.op(*mapped_args)
+    def remap(self, *args: QSystemStruct) -> QSystemStruct:
+        return self.func(*args)
+
+    def __repr__(self) -> str:
+        return f"RemappedByLambda({self.op}, {self.func})"
+
+
+class RemappedByIndices(Remapped[Op]):
+    def __init__(self, op: Op, *indices: int, name: Optional[str] = None):
+        super().__init__(op, name=name)
+        self._indices = indices
+
+    @property
+    def indices(self) -> tuple[int, ...]:
+        return self._indices
+
+    def remap(self, *args: QSystemStruct) -> QSystemStruct:
+        return tuple(args[i] for i in self._indices)
+
+    def __repr__(self) -> str:
+        return f"RemappedByIndices({self.op}, {', '.join(map(str, self._indices))})"
