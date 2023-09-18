9e989d047f453ae8bed3c89a459702a4143dc6d3
keli <zhengkeli2009@126.com>
2023-09-18 15:40:17 +0800

    optimized op_impl.py, added registry key OpInst

--- braandket_circuit/traits/op_impl.py
+++ braandket_circuit/traits/op_impl.py
@@ -1,8 +1,7 @@
 from typing import Any, Callable, TypeVar, overload
 
 from braandket_circuit.basics import QOperation, QRuntime, QSystemStruct, R
-from zkl_registries import SimpleRegistry
-from zkl_registries.tagkeys.type import SupTypeTagKey
+from zkl_registries import ObjTagKey, SimpleRegistry, SupTypeTagKey
 
 Rt = TypeVar('Rt', bound=QRuntime)
 Op = TypeVar('Op', bound=QOperation)
@@ -10,13 +9,14 @@ BnkRuntimeOpImpl = Callable[[Rt, Op, QSystemStruct], R]
 
 RtType = SupTypeTagKey[type[QRuntime], type[QRuntime]]('RuntimeType')
 OpType = SupTypeTagKey[type[QOperation], type[QOperation]]('OperationType')
-_op_impl_registry = SimpleRegistry[BnkRuntimeOpImpl, Any, Any](keys=[RtType, OpType])
+OpInst = ObjTagKey[QOperation]('OperationInstance', strict=False, required=False)
+_op_impl_registry = SimpleRegistry[BnkRuntimeOpImpl, Any, Any](keys=[RtType, OpType, OpInst])
 
 
 @overload
 def register_op_impl(
-    rt_type: type[Rt] | None,
-    op_type: type[Op] | None,
+    rt: type[Rt] | Rt | None,
+    op: type[Op] | Op | None,
     impl: BnkRuntimeOpImpl
 ) -> BnkRuntimeOpImpl:
     pass
@@ -24,30 +24,47 @@ def register_op_impl(
 
 @overload
 def register_op_impl(
-    rt_type: type[Rt] | None,
-    op_type: type[Op] | None,
+    rt: type[Rt] | Rt | None,
+    op: type[Op] | Op | None,
 ) -> Callable[[BnkRuntimeOpImpl], BnkRuntimeOpImpl]:
     pass
 
 
 def register_op_impl(
-    rt_type: type[Rt] | None,
-    op_type: type[Op] | None,
+    rt: type[Rt] | Rt | None,
+    op: type[Op] | Op | None,
     impl: BnkRuntimeOpImpl | None = None
 ) -> BnkRuntimeOpImpl | Callable[[BnkRuntimeOpImpl], BnkRuntimeOpImpl]:
     if impl is None:
         def decorator(impl: BnkRuntimeOpImpl):
-            register_op_impl(rt_type, op_type, impl)
+            register_op_impl(rt, op, impl)
             return impl
 
         return decorator
 
-    _op_impl_registry.register(impl, {RtType: rt_type, OpType: op_type})
+    if not isinstance(rt, type):
+        rt = type(rt)
+
+    if not isinstance(op, type):
+        op_type = type(op)
+    else:
+        op_type = op
+        op = None
+
+    _op_impl_registry.register(impl, {RtType: rt, OpType: op_type, OpInst: op})
     return impl
 
 
 def get_op_impls(
-    rt_type: type[Rt] | None,
-    op_type: type[Op] | None,
+    rt: type[Rt] | Rt | None,
+    op: type[Op] | Op | None,
 ) -> tuple[BnkRuntimeOpImpl, ...]:
-    return _op_impl_registry.match({RtType: rt_type, OpType: op_type})
+    if not isinstance(rt, type):
+        rt = type(rt)
+    if not isinstance(op, type):
+        op_type = type(op)
+    else:
+        op_type = op
+        op = None
+
+    return _op_impl_registry.match({RtType: rt, OpType: op_type, OpInst: op})
