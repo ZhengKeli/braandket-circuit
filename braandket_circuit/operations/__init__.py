c6879d9f92ddb1efb077c7dcf08cab66bdef5210
keli <zhengkeli2009@126.com>
2023-09-18 21:00:22 +0800

    moved qubit_gates to upper-level folder

--- braandket_circuit/operations/__init__.py
+++ braandket_circuit/operations/__init__.py
@@ -1,4 +1,4 @@
-from .gates import *
+from .gates import CX, CY, CZ, D, H, I, M, NOT, Rx, Ry, Rz, S, T, X, Y, Z
 from .identity import Identity
 from .matrix import MatrixOperation, QubitsMatrixOperation
 from .measurement import DesiredMeasurement, MeasurementResult, ProjectiveMeasurement
--- braandket_circuit/operations/gates/__init__.py
+++
@@ -1,1 +0,0 @@
-from .qubit_gates import CX, CY, CZ, H, I, NOT, Rx, Ry, Rz, S, T, X, Y, Z
