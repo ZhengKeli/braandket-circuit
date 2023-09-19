340d01570adae56fc5dec34d8ca0d4327fc1afb2
keli <zhengkeli2009@126.com>
2023-09-19 13:41:37 +0800

    optimized gates.py

--- braandket_circuit/operations/gates.py
+++ braandket_circuit/operations/gates.py
@@ -1,53 +1,51 @@
 import abc
+from typing import Optional
 
 from braandket import ArrayLike
 from braandket_circuit.basics import QOperation, QParticle
 
 
-# single qubit constant gates
-
-class _SingleQubitConstantGate(QOperation):
+class _SingleQubitGate(QOperation, abc.ABC):
     def __call__(self, qubit: QParticle):
         return super().__call__(qubit)
 
-    def __repr__(self):
-        return f"{self.name}"
 
+# constant gates
 
-class PauliXGate(_SingleQubitConstantGate):
+class PauliXGate(_SingleQubitGate):
     def __init__(self):
         super().__init__(name="X")
 
 
-class PauliYGate(_SingleQubitConstantGate):
+class PauliYGate(_SingleQubitGate):
     def __init__(self):
         super().__init__(name="Y")
 
 
-class PauliZGate(_SingleQubitConstantGate):
+class PauliZGate(_SingleQubitGate):
     def __init__(self):
         super().__init__(name="Z")
 
 
-class HalfPiPhaseGate(_SingleQubitConstantGate):
+class HalfPiPhaseGate(_SingleQubitGate):
     def __init__(self):
         super().__init__(name="S")
 
 
-class QuarterPiPhaseGate(_SingleQubitConstantGate):
+class QuarterPiPhaseGate(_SingleQubitGate):
     def __init__(self):
         super().__init__(name="T")
 
 
-class HadamardGate(_SingleQubitConstantGate):
+class HadamardGate(_SingleQubitGate):
     def __init__(self):
         super().__init__(name="H")
 
 
-# single qubit rotation gates
+# rotation gates
 
-class _SingleQubitRotationGate(QOperation, abc.ABC):
-    def __init__(self, theta: ArrayLike, *, name: str):
+class _SingleQubitRotationGate(_SingleQubitGate, abc.ABC):
+    def __init__(self, theta: ArrayLike, *, name: Optional[str] = None):
         super().__init__(name=name)
         self._theta = theta
 
@@ -55,11 +53,11 @@ class _SingleQubitRotationGate(QOperation, abc.ABC):
     def theta(self) -> ArrayLike:
         return self._theta
 
-    def __call__(self, qubit: QParticle):
-        return super().__call__(qubit)
-
-    def __repr__(self) -> str:
+    def __str__(self):
         return f'{self.name}({self.theta})'
+
+    def __repr__(self):
+        return f'{type(self).__name__}({self.theta})'
 
 
 class RotationXGate(_SingleQubitRotationGate):
