127606687b0adfacde1a3c3d964a505f01254d35
keli <zhengkeli2009@126.com>
2023-09-21 13:13:14 +0800

    added __len__, __getitem__, __add__, __radd__,__mul__ for Sequential, removed property Sequential.steps

--- braandket_circuit/operations/sequential.py
+++ braandket_circuit/operations/sequential.py
@@ -1,4 +1,5 @@
-from typing import Generic, Iterable, Optional, TypeVar
+from copy import copy
+from typing import Generic, Iterable, Iterator, Optional, TypeVar, Union, overload
 
 from braandket_circuit.basics import QOperation, QSystemStruct, R
 
@@ -8,27 +9,49 @@ Op = TypeVar('Op', bound=QOperation)
 class Sequential(QOperation[tuple], Generic[Op]):
     def __init__(self, steps: Iterable[Op], *, name: Optional[str] = None):
         super().__init__(name=name)
+        self._steps = tuple(steps)
 
-        # check
-        steps = tuple(steps)
-        for i, step in enumerate(steps):
-            if not isinstance(step, QOperation):
-                raise TypeError(f"steps[{i}]={step} is not a QOperation!")
+    def __len__(self):
+        return len(self._steps)
 
-        self._steps = steps
+    def __iter__(self) -> Iterator[Op]:
+        return iter(self._steps)
 
-    @property
-    def steps(self) -> tuple[Op, ...]:
-        return self._steps
+    @overload
+    def __getitem__(self, item: int) -> Op:
+        pass
+
+    @overload
+    def __getitem__(self, item: slice) -> 'Sequential[Op]':
+        pass
+
+    def __getitem__(self, item: int | slice) -> Union[Op, 'Sequential[Op]']:
+        if isinstance(item, slice):
+            new = copy(self)
+            new._steps = new._steps[item]
+            return new
+        return self._steps[item]
+
+    def __add__(self, other: Op) -> 'Sequential[Op]':
+        if isinstance(other, Sequential) and other.name is None:
+            return Sequential((*self, *other), name=self.name)
+        else:
+            return Sequential((*self, other), name=self.name)
+
+    def __radd__(self, other: Op) -> 'Sequential[Op]':
+        return Sequential((other, *self), name=self.name)
+
+    def __mul__(self, other: int) -> 'Sequential[Op]':
+        return Sequential([self] * other, name=self.name)
 
     def __call__(self, *args: QSystemStruct) -> R:
         results = []
-        for step in self.steps:
+        for step in self:
             results.append(step(*args))
         return tuple(results)
 
     def __repr__(self) -> str:
-        s = f"Sequential(["
-        for step in self.steps:
+        s = f"{type(self).__name__}(["
+        for step in self:
             s += f"\n\t" + repr(step).replace("\n", "\n\t") + ","
         return s + "\n])"
