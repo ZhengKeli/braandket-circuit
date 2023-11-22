25e59147e7d28d23eb742f5b8473edee8c5ea8ff
keli <zhengkeli2009@126.com>
2023-11-22 17:48:06 +0800

    added unittest for freeze()

---
+++ tests/traits/freeze/test_freeze.py
@@ -0,0 +1,21 @@
+from braandket_circuit import CNOT, Controlled, H, QOperation, QParticle, RemappedByIndices, Sequential, X, apply, \
+    freeze
+
+
+class CustomGate(QOperation[None]):
+    def __call__(self, q0: QParticle, q1: QParticle):
+        H(q0)
+        CNOT(q0, q1)
+
+
+def test_freeze_custom_gate():
+    print(apply)
+    gate = CustomGate()
+    frozen = freeze(gate)
+    assert isinstance(frozen, Sequential)
+    assert len(frozen) == 2
+    assert isinstance(frozen[0], RemappedByIndices)
+    assert frozen[0].op is H
+    assert isinstance(frozen[1], RemappedByIndices)
+    assert isinstance(frozen[1].op, Controlled)
+    assert frozen[1].op.op is X
