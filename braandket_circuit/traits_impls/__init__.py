17e60a9dd6ab3eccae72947ccaa11bd6a3519495
keli <zhengkeli2009@126.com>
2023-11-30 15:49:53 +0800

    removed useless dynamic import of .compile in traits_impls

--- braandket_circuit/traits_impls/__init__.py
+++ braandket_circuit/traits_impls/__init__.py
@@ -1,7 +1,2 @@
-import importlib
-
 from .apply import BnkParticle, BnkRuntime, BnkState, SymbolicParticle, SymbolicRuntime
 from .compile import FreezePass
-
-importlib.import_module(".compile", __package__)
-del importlib
